a607f2936a3631df82bbe7590df12a3591a7e0ee
Seif <seifjlassi444@gmail..com>
2023-04-26 11:17:52 +0200

    com_msg

--- read.py
+++ read.py
@@ -20,7 +20,7 @@ try:
         # extract the Ethernet protocol type from the frame
         eth_type = int.from_bytes(frame[12:14], byteorder="big")
         if eth_type == 34980:  # 0x88a4:
-            print(frame[14:17])
+            print(frame[14:16])
             # eth_cat_header = int.from_bytes(frame[14:17], byteorder="big")
             # print(bin(int.from_bytes(src_mac, byteorder="big")), 6)
             # print(bin(eth_cat_header), 2)
